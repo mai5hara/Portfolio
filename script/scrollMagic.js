6d91d033d734ec9fd7f5c88452766c8d705eda49
maigohara <maigohara-1103@outlook.jp>
2020-09-17 16:52:00 -0700

    Organized styles of about page

--- script/scrollMagic.js
+++ script/scrollMagic.js
@@ -1,85 +1,85 @@
 let controller = new ScrollMagic.Controller();
 
 toggleHtml = () => {
-let scene = new ScrollMagic.Scene({
-    triggerElement: '.skills__percentage__html',
-    offset: -400
-})
-    .setClassToggle('.skills__percentage__html', 'percentage__html')
-    .addTo(controller);
+    let scene = new ScrollMagic.Scene({
+        triggerElement: '.about-skill__percentage-html',
+        offset: -400
+    })
+        .setClassToggle('.about-skill__percentage-html', 'about-skill__percentage--html')
+        .addTo(controller);
 }
 
 togglePs = () => {
     let scene = new ScrollMagic.Scene({
-        triggerElement: '.skills__percentage__ps',
+        triggerElement: '.about-skill__percentage-ps',
         offset: -400
     })
-        .setClassToggle('.skills__percentage__ps', 'percentage__ps')
+        .setClassToggle('.about-skill__percentage-ps', 'about-skill__percentage--ps')
         .addTo(controller);
-    }
+}
 
 toggleAi = () => {
     let scene = new ScrollMagic.Scene({
-        triggerElement: '.skills__percentage__ai',
+        triggerElement: '.about-skill__percentage-ai',
         offset: -400
     })
-        .setClassToggle('.skills__percentage__ai', 'percentage__ai')
+        .setClassToggle('.about-skill__percentage-ai', 'about-skill__percentage--ai')
         .addTo(controller);
-    }
+}
 
 togglePr = () => {
     let scene = new ScrollMagic.Scene({
-        triggerElement: '.skills__percentage__pr',
+        triggerElement: '.about-skill__percentage-pr',
         offset: -400
     })
-        .setClassToggle('.skills__percentage__pr', 'percentage__pr')
+        .setClassToggle('.about-skill__percentage-pr', 'about-skill__percentage--pr')
         .addTo(controller);
-    }
+}
 
 toggleJs = () => {
     let scene = new ScrollMagic.Scene({
-        triggerElement: '.skills__percentage__js',
+        triggerElement: '.about-skill__percentage-js',
         offset: -400
     })
-        .setClassToggle('.skills__percentage__js', 'percentage__js')
+        .setClassToggle('.about-skill__percentage-js', 'about-skill__percentage--js')
         .addTo(controller);
-    }
+}
 
 toggleSketch = () => {
     let scene = new ScrollMagic.Scene({
-        triggerElement: '.skills__percentage__sketch',
+        triggerElement: '.about-skill__percentage-sketch',
         offset: -400
     })
-        .setClassToggle('.skills__percentage__sketch', 'percentage__sketch')
+        .setClassToggle('.about-skill__percentage-sketch', 'about-skill__percentage--sketch')
         .addTo(controller);
-    }
+}
 
 toggleSass = () => {
     let scene = new ScrollMagic.Scene({
-        triggerElement: '.skills__percentage__sass',
+        triggerElement: '.about-skill__percentage-sass',
         offset: -400
     })
-        .setClassToggle('.skills__percentage__sass', 'percentage__sass')
+        .setClassToggle('.about-skill__percentage-sass', 'about-skill__percentage--sass')
         .addTo(controller);
-    }
+}
 
 toggleReact = () => {
     let scene = new ScrollMagic.Scene({
-        triggerElement: '.skills__percentage__react',
+        triggerElement: '.about-skill__percentage-react',
         offset: -400
     })
-        .setClassToggle('.skills__percentage__react', 'percentage__react')
+        .setClassToggle('.about-skill__percentage-react', 'about-skill__percentage--react')
         .addTo(controller);
-    }
+}
 
 toggleAf = () => {
     let scene = new ScrollMagic.Scene({
-        triggerElement: '.skills__percentage__af',
+        triggerElement: '.about-skill__percentage-af',
         offset: -400
     })
-        .setClassToggle('.skills__percentage__af', 'percentage__af')
+        .setClassToggle('.about-skill__percentage-af', 'about-skill__percentage--af')
         .addTo(controller);
-    }
+}
 
 toggleAi();
 togglePr();
